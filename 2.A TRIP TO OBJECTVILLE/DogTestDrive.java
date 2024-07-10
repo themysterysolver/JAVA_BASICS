dcfb13c4f9fe39eb02eb5b2b72935a1a85f474ea
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-10 22:18:15 +0530

    CHORE:new folders for chapters is created

---
+++ 2.A TRIP TO OBJECTVILLE/DogTestDrive.java
@@ -0,0 +1,15 @@
+class Dog{
+    int size;
+    String breed;
+    String name;
+    void bark(){
+        System.out.println("LOL!LOL!");
+    }
+}
+public class DogTestDrive{
+    public static void main(String[] arg){
+        Dog d=new Dog();
+        d.bark(); 
+
+    }
+}
