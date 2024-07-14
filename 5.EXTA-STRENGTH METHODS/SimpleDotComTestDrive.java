f827d7a663ddefbb38c4157ca58c2db870a8a24e
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-14 22:42:19 +0530

    CHORE-New folder for extra string methods

---
+++ 5.EXTA-STRENGTH METHODS/SimpleDotComTestDrive.java
@@ -0,0 +1,14 @@
+//writing 1st test code for SimpleDotCom class
+class SimpleDotComTestDrive{
+    public static void main(Strting[] args){
+        int[] location={1,2,3} //consecutive location for a single SHIP===dotcom
+        SimpleDotCom dot=new SimpleDotCom(location);//creating a location in array
+        String guess="2"; 
+        String result=dot.ChekGuess(guess);
+        Boolean testresult;
+        if(result=='Hit' or result.equal('Hit')){
+            testresult=true;
+        }
+        System.out.result("THE TEST RESULT:"+testresult);
+    }
+}
