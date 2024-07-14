6873f70a86520777d57bc21058c1feed3853ff31
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-14 23:23:59 +0530

    FEAT:class SimpleDotCom with methods checkguess and setlocation

--- 5.EXTA-STRENGTH METHODS/SimpleDotComTestDrive.java
+++ 5.EXTA-STRENGTH METHODS/SimpleDotComTestDrive.java
@@ -1,15 +1,38 @@
-//writing 1st test code for SimpleDotCom class
+//class for the before test.ie)SimpleDotCom class
+class SimpleDotCom{
+    int[] locations;
+    int NoHits=0;
+    void SetLocation(int[] loc){
+        locations=loc;
+    }
+    String ChekGuess(String guess){
+        String result="Miss";
+        int guessint=Integer.parseInt(guess);
+        for(int cell:locations){ //int cells: //should initialse the iteratble!
+            if(cell==guessint){
+                NoHits++;
+                result="Hit";
+                break;
+            }
+            if(NoHits==locations.length){
+                result="kill";
+            }
+        }
+        return result;
+    }
+}
+
 class SimpleDotComTestDrive{
-    public static void main(Strting[] args){
-        int[] location={1,2,3} //consecutive location for a single SHIP===dotcom
+    public static void main(String[] args){
+        int[] location={1,2,3}; //consecutive location for a single SHIP===dotcom
         SimpleDotCom dot=new SimpleDotCom();//creating a location in array
         dot.SetLocation(location);
         String guess="2"; 
         String result=dot.ChekGuess(guess);
-        Boolean testresult;
-        if(result=='Hit' or result.equal('Hit')){
+        Boolean testresult=false;
+        if(result.equals("Hit")){
             testresult=true;
         }
-        System.out.result("THE TEST RESULT:"+testresult);
+        System.out.println("THE TEST RESULT:"+testresult);
     }
 }
