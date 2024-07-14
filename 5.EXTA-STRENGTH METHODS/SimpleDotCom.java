8a1b5b64084f82578035dd26d9d9ac975ebdbd8d
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-14 22:39:24 +0530

    FEAT:SimpleDotCom TestDrive-TEST CODE

--- 5.EXTA-STRENGTH METHODS/SimpleDotCom.java
+++
@@ -1,7 +0,0 @@
-//step-1:WRITING PREP CODE--consist of instance variables,methods,method logic!
-//step-2:WRITING TEST CODE
-//step-3:WRITING REAL CODE
-
-//writing test code first is a practise of XP-Extreme Programming
-
-
