6b8e8138159ad1c399ddb8d78843580881d76bde
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-19 10:44:19 +0530

    NEW:Realy Battleship=Dotcom game layout

---
+++ 6.USING THE JAVA LIBRARY/RealGameDot.java
@@ -0,0 +1,20 @@
+//GAME IDEA:
+class DotCom{
+    //setname(),ie)To create 3DotComs=Ships,name them!!
+    //setLocations()
+    //checkGuess()
+}
+class GameHelper{
+    //placedotcom(),getuserinput()-Helps to get user input also to set locations for those three ships=DotComs
+}
+class RealGameDot{
+    public static void main(String[] args){
+        //Main function
+        //dotComsList-create arraylist to hod three DotCom object
+        /*setupgame()-then each DotCom object(iteratively for elemnts in list) 
+        should hold the location of those DotComs with help of placeDotCom()*/
+        /*playingGame()-getUserInput() from helper then cherUserGuess*/
+        /* checkUserGuess()-hit,miss,kill*/
+        /*finishgame()*/
+    }
+}
