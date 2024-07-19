eb4c527ae603b845f0b5d26e3ed723cde7d821c9
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-19 15:47:26 +0530

    FEAT:method setGame(),didn't test!

--- 6.USING THE JAVA LIBRARY/RealGameDot.java
+++ 6.USING THE JAVA LIBRARY/RealGameDot.java
@@ -1,6 +1,10 @@
-//GAME IDEA:
+
 class DotCom{
     //setname(),ie)To create 3DotComs=Ships,name them!!
+    String name;
+    void setName(String nam){
+        name=nam;
+    }
     //setLocations()
     //checkGuess()
 }
@@ -9,12 +13,36 @@ class GameHelper{
 }
 class RealGameDot{
     //dotComsList-create arraylist to hod three DotCom object
+    private ArrayList<DotCom> dotComList=new ArrayList<DotCom>();
+    private int NoOfGuesse=0;
+    private GameHelper helper=new GameHelper();
     /*setupgame()-then each DotCom object(iteratively for elemnts in list) 
     should hold the location of those DotComs with help of placeDotCom()*/
+    void setupGame(){
+        DotCom one=new DotCom();
+        DotCom two=new DotCom();
+        DotCom three=new DotCom();
+        one.setName("google.com");
+        two.setName("youtube.com");
+        three.setName("wow.com");
+        dotComList.add(one);
+        dotComList.add(two);
+        dotComList.add(three);
+        for(DotCom Dcoms:dotComList){
+            ArrayList<String> loc=helper.placedDotCom(3);
+            Dcoms.setLocations(loc);
+        }
+        System.out.println("WELCOME TO SINK DOTCOM GAME!!");
+        System.out.println("Sink the 3 ships named:"+one.name+","+twon.name+","three.name+" in 7*7 MATRIX CELL");
+        System.out.println("Sink the ships in minimum no of guesses");
+    }
     /*playingGame()-getUserInput() from helper then cherUserGuess*/
     /* checkUserGuess()-hit,miss,kill*/
     /*finishgame()*/
     public static void main(String[] args){
-        //Main function
+        //Main function call!!
+        RealGameDot game=new RealGameDot();
+        game.setupGame();
+        game.playGame();
     }
 }
