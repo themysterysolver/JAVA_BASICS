96bd7bc1d480f9cbf1ae2bdfd79004a914b27b00
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-21 22:39:45 +0530

    NEW:Intro to polymorphism

---
+++ 7.INHERITANCE AND POLYMORPHISM/Poly.java
@@ -0,0 +1,8 @@
+/*Polymorphism tells that during a  object creation,
+  ref varaible and object can be of different types such that object is a subclass and ref var is supertype*/
+  //object IS-A reference variable
+//this agree with arguments,return values also!!
+//referenc variable of superclass!
+//we can't extend any class private,final,those not in same package even if it's public
+//OVERRIDING:-same arguments and return types,can't be less acessible!!
+//OVERLOADING:-new arguments of diff types can be added,return type can be be different iff arguments have changes,vary access modifiers
