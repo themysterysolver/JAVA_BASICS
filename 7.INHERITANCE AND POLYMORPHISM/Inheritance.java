4b2c8a4af2825b607fbc2ab10d7c24ef3a2d405e
themysterysolver <arjunprabhakar1910@gmail.com>
2024-07-21 10:23:23 +0530

    CHORE:INTRO to INHERTITANCE CONCPETS

---
+++ 7.INHERITANCE AND POLYMORPHISM/Inheritance.java
@@ -0,0 +1,13 @@
+//SUPERCLASS,SUBCLASS
+//With inheritance u can use memebers(instance varaibles,methods!!)
+//subclass EXTENDS from super class
+//EXTEND is a keyword!
+//we can override in methods of superclass in subclass and also add new functions!
+//IS-A used when subclass iherits a any of superclass.ie)all SUBCLASS of a super classclass IS-A SUPERCLASS!
+//JVM starts comilation at the lowest class.Then move upwards in INHERITANCE-TREE!
+//To check TYPE we can use HAS-A, does class-A HAS-A class-B...ie)class-A has an object of CLASS-B.
+class Inheritance{
+    public static void main(String[] args){
+        
+    }
+}
