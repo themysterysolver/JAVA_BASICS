208b7493fe7dfd90c7dc318c9d37321e25eb4ed8
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-07 22:29:12 +0530

    Basic msging txt has been created!

--- WEEKLY_LAB_EXERCISE/WEEK 1/Msg.java
+++ WEEKLY_LAB_EXERCISE/WEEK 1/Msg.java
@@ -1,8 +1,32 @@
-import java.utils.*;
+import java.util.*;
 class user{
+    String username;
+    private static  int count=0;
+    Stack<String> sentBox;
+    Stack<String> inBox;
     public user(){
-        
+        Scanner input=new Scanner(System.in);
+        System.out.print("you are user:"+count+"\nEnter your username:");
+        username=input.nextLine();
+        this.sentBox=new Stack<>();
+        this.inBox=new Stack<>();
+        count++;
     }
-    sendBox
-    inBox
+    void historySentBox(){
+        for(int i=0;i<sentBox.size();i++){
+            System.out.println(i+"Sent to:"+sentBox.get(i));
+        }
+    }
+    void historyinBox(){
+        for(int i=0;i<sentBox.size();i++){
+            System.out.println(i+"Recieved from:"+sentBox.get(i));
+        }
+    }
+    
+}
+class Msg{
+    public static void main(String[]  args){
+        user a=new user();
+        user b=new user();
+    }
 }
