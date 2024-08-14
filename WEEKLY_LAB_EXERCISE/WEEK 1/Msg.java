e545aa4e2a8f5d2809b345ab9cee2b24be53f72c
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-14 05:44:40 +0530

    FEAT:completed PRIMITIVE chat between 2 users using setters and getters

--- WEEKLY_LAB_EXERCISE/WEEK 1/Msg.java
+++ WEEKLY_LAB_EXERCISE/WEEK 1/Msg.java
@@ -1,32 +1,41 @@
 import java.util.*;
 class user{
-    String username;
-    private static  int count=0;
-    Stack<String> sentBox;
-    Stack<String> inBox;
-    public user(){
-        Scanner input=new Scanner(System.in);
-        System.out.print("you are user:"+count+"\nEnter your username:");
-        username=input.nextLine();
-        this.sentBox=new Stack<>();
-        this.inBox=new Stack<>();
-        count++;
+    private String username;
+    private String sMsg,rMsg;
+    user(String s){
+        username=s;
     }
-    void historySentBox(){
-        for(int i=0;i<sentBox.size();i++){
-            System.out.println(i+"Sent to:"+sentBox.get(i));
-        }
+    void setsMsg(String a){
+        sMsg=a;
+        System.out.println(username+" SENT: "+a);
     }
-    void historyinBox(){
-        for(int i=0;i<sentBox.size();i++){
-            System.out.println(i+"Recieved from:"+sentBox.get(i));
-        }
+    void setrcvMsg(String b){
+        rMsg=b;
+        System.out.println(username+" RECIEVED: "+b);
     }
-    
+    String getMsg(){
+        return sMsg;
+    }
 }
 class Msg{
     public static void main(String[]  args){
-        user a=new user();
-        user b=new user();
+        System.out.print("PRIMITIVE CHAT APPLICATION!");
+        Scanner input=new Scanner(System.in);
+        user a=new user("USER1");
+        user b=new user("USER2");
+        while(true){
+            System.out.print("\nSEND MESSAGE FROM USER-1 TO  USER-2:");
+            a.setsMsg(input.nextLine());
+            b.setrcvMsg(a.getMsg());
+            System.out.println("\nSEND MESSAGE FROM   USER-2 TO USER-1:");
+            b.setsMsg(input.nextLine());
+            a.setrcvMsg(b.getMsg());
+            System.out.println("Press N to break!");
+            char c=input.nextLine().charAt(0);
+            if(c=='N'){
+                System.out.println("-----------------------CHAT ENDS----------------------");
+                break;
+            }
+        }
     }
 }
