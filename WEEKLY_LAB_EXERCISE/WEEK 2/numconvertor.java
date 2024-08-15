bfaab5d4e66e1727e29d237a41bfb1c526fa4266
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-15 05:50:12 +0530

    FEAT:Getting input!

--- WEEKLY_LAB_EXERCISE/WEEK 2/numconvertor.java
+++ WEEKLY_LAB_EXERCISE/WEEK 2/numconvertor.java
@@ -1,3 +1,20 @@
+import java.util.*;
 class numconvertor{
-    
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a 3 digit number from 0 to 999:");
+        int num=input.nextInt();
+        while(num>999 || num<0){
+            System.out.print("Enter a VALID 3 digit number from 0 to 999:");
+            num=input.nextInt();
+        }
+        ArrayList<Integer> n=new ArrayList<>();
+        int a;
+        while(num!=0){
+            a=num%10;
+            n.add(0,a);
+            num=num/10;
+        }
+        System.out.println(n);
+    }
 }
