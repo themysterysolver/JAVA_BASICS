3d93bbbcd3ea38f71a12d9df5c7983fb32043934
themysterysolver <arjunprabhakar1910@gmail.com>
2024-09-04 19:20:44 +0530

    FIX:semi fix of logic

--- WEEKLY_LAB_EXERCISE/WEEK-5/compression.java
+++ WEEKLY_LAB_EXERCISE/WEEK-5/compression.java
@@ -2,19 +2,19 @@ import java.util.*;
 class strIntro{
     public static void main(String[] args){
         System.out.println("R.Prabhakara Arjun\n2022503003\n");
-        String s1="aabbcccdddaabbccccc";
+        String s1="aabbcccdddaabbc";
         String finale="";
         for(int i=0;i<s1.length();i++){
             finale=finale+s1.charAt(i);
             int count=1;
             for(int j=i+1;j<s1.length();j++){
-                if(s1.charAt(i)==s1.charAt(j) ){
-                    count++;
-                }
-                else if(j==s1.length()-1){
+                if(j==s1.length()){
                     i=j;
                     finale=finale+String.valueOf(count);
                     break;
+                }
+                if(s1.charAt(i)==s1.charAt(j) ){
+                    count++;
                 }
                 else{
                     i=j;
