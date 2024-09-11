9a0c903f7c1e1da6b74b5ab10642af8922ca98b6
themysterysolver <arjunprabhakar1910@gmail.com>
2024-09-11 18:42:53 +0530

    FEAT:complex op updated!

--- WEEKLY_LAB_EXERCISE/WEEK-6/J56_3003.java
+++ WEEKLY_LAB_EXERCISE/WEEK-6/J56_3003.java
@@ -27,16 +27,31 @@ class complex{
             return real+"-"+Math.abs(imaginary)+"i";
         }
     }
-    static complex add(complex a,complex b){
+    complex add(complex b){
         complex c=new complex();
-        c.setReal(a.getReal()+b.getReal());
-        c.setImaginary(a.getImaginary()+b.getImaginary());
+        c.setReal(this.getReal()+b.getReal());
+        c.setImaginary(this.getImaginary()+b.getImaginary());
         return c;
     }
-    static complex sub(complex a,complex b){
+    complex sub(complex b){
         complex c=new complex();
-        c.setReal(a.getReal()-b.getReal());
-        c.setImaginary(a.getImaginary()-b.getImaginary());
+        c.setReal(this.getReal()-b.getReal());
+        c.setImaginary(this.getImaginary()-b.getImaginary());
+        return c;
+    }
+    complex multiplication(complex b){
+        complex c=new complex();
+        c.setReal(this.getReal()*b.getReal()-this.getImaginary()*b.getImaginary());
+        c.setImaginary(this.getReal()*b.getImaginary()+b.getReal()*this.getImaginary());
+        return c;
+    }
+    Boolean equals(complex c){
+        return this.getReal()==c.getReal() && this.getImaginary()==c.getImaginary();
+    }
+    complex divide(complex b){
+        complex c=new complex();
+        c.setReal((this.getReal()*b.getReal()+this.getImaginary()*b.getImaginary())/(b.getReal()*b.getReal()+b.getImaginary()*b.getImaginary()));
+        c.setImaginary((this.getReal()*b.getImaginary()-b.getReal()*this.getImaginary())/(b.getReal()*b.getReal()+b.getImaginary()*b.getImaginary()));
         return c;
     }
 }
@@ -45,15 +60,20 @@ public class J56_3003 {
         complex a=new complex();
         complex b=new complex();
         System.out.println("The complex no:"+a.getReal()+"+"+a.getImaginary()+"i");
-        a.setReal(100);a.setImaginary(15);
-        b.setReal(15);b.setImaginary(69);
+        a.setReal(10);a.setImaginary(15);
+        b.setReal(10);b.setImaginary(15);
         a.display();
         System.out.println("The complex no a:"+a);
         System.out.println("The complex no b:"+b);
-        complex sum=complex.add(a,b);
-        System.out.println("a+b:"+sum);
-        complex sub=complex.sub(a,b);
-        System.out.println("a-b:"+sub);
-
+        complex sum=a.add(b);
+        System.out.println("complex no addition(a+b):"+sum);
+        complex sub=a.sub(b);
+        System.out.println("complex no difference(a-b):"+sub);
+        complex mul=a.multiplication(b);
+        System.out.println("Complex no multiplication(a*b):"+mul);
+        complex quo=a.divide(b);
+        System.out.println("Complex no quotient(a/b):"+quo);
+        Boolean isEqual=a.equals(b);
+        System.out.println("a.equals(b):"+isEqual); 
     }
 }
