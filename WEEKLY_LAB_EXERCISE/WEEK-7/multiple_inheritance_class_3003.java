1064345e7c83db0ed5c9f4eadbf7ed155a58e995
themysterysolver <arjunprabhakar1910@gmail.com>
2024-09-18 20:20:15 +0530

    FIX:erro,clas can't be multiply inherited!

---
+++ WEEKLY_LAB_EXERCISE/WEEK-7/multiple_inheritance_class_3003.java
@@ -0,0 +1,18 @@
+class plants{
+    String scientificName="";
+    String modeOfNutrietent="";
+}
+class organsism_combo  extends animal,plants{
+    void display(){
+        System.out.println("This won't even run!!");
+    }
+}
+public class multiple_inheritance_class_3003{
+    public static void main(String[] args){
+        
+        organsism_combo org=new organsism_combo();
+        org.display();
+
+        System.out.println("This won't even run!!");
+    }
+}
