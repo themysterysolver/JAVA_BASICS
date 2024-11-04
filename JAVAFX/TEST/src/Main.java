81174ab8d0db28d6a8610a20f041f0ba8593da5d
themysterysolver <arjunprabhakar1910@gmail.com>
2024-11-04 18:57:50 +0530

    FEAT:my first TEST FX program!

---
+++ JAVAFX/TEST/src/Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Example");
+
+        Button button = new Button("Click Me!");
+        button.setOnAction(e -> System.out.println("Button clicked!"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
