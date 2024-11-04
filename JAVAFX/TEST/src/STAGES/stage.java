78b54f0d29437d34328d9074c3826f7063951ef2
themysterysolver <arjunprabhakar1910@gmail.com>
2024-11-04 19:41:19 +0530

    FEAT:setTitle() and Image at top and windows side!

--- JAVAFX/TEST/src/STAGES/stage.java
+++ JAVAFX/TEST/src/STAGES/stage.java
@@ -4,17 +4,20 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class stage extends Application{
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage stage) throws Exception {
         //Stage s=new Stage(); //Creating a new stage!!
         Group root=new Group();
         Scene scene=new Scene(root, Color.BLACK);
+        stage.setTitle("My first STAGE!!");
+        Image icon=new Image("hello.png");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
