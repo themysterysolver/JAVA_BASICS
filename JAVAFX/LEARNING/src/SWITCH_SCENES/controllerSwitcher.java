49f686cca178c57ba2a12e39fc20c0a82517f093
themysterysolver <arjunprabhakar1910@gmail.com>
2024-11-10 17:45:14 +0530

    FEAT:learnt swithcing between scenes

---
+++ JAVAFX/LEARNING/src/SWITCH_SCENES/controllerSwitcher.java
@@ -0,0 +1,30 @@
+package SWITCH_SCENES;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class controllerSwitcher {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchToScene1(ActionEvent event) throws IOException{
+        Parent root= FXMLLoader.load(getClass().getResource("scene1.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene2(ActionEvent event) throws IOException{
+        Parent root= FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ JAVAFX/LEARNING/src/SWITCH_SCENES/switchScene.java
@@ -0,0 +1,26 @@
+package SWITCH_SCENES;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class switchScene extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+            Parent root= FXMLLoader.load(getClass().getResource("scene1.fxml"));
+            Scene scene=new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
